16ce840d1f7eac6fa434f318af52372f1ac00151
ljiang-git <royflorajiang@gmail.com>
2025-01-13 18:06:31 -0500

    etch-a-sketch game

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+screen.fill("white")
+
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.draw.circle(screen, "blue", player_pos, 10)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_DOWN]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+    if keys[pygame.K_c]:
+        screen.fill("white")
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
